f16956b7a36b88492f4c0c58c841788a443f5d70
Alejandro Harb <alejandroharb@gmail.com>
2017-04-12 17:26:36 -0500

    changes to bat sprite

--- public/script/batSwing.js
+++ public/script/batSwing.js
@@ -1,24 +1,47 @@
-function batSwing() {
+restoreBatPos = () => {
+  // const dx = 3; //x dimension of image
+  // const dy = 0.25; //y dimension of image
+  // const transY = dx * tan(radians(calcAngle)); //image y displacement depending on angle of rotation
+  // const transX = -dy / (tan(radians(calcAngle))); //image X displacement depending on angle of rotation
+  // for (var i = swingAngle; i < 0; i++) {
+  //   swingAngle += 5
+  //   bat.position.x += transX; //moves image in x position, (0.01 to decrease magnitude**testing***)
+  //   bat.position.y += transY; //moves image in y position, (magnitude testing)
+  //   bat.position.y = constrain(bat.position.y, bat.position.y - 0.05, bat.position.y + 6.4)
+  //   bat.position.x = constrain(bat.position.x, bat.position.x - 0.11, bat.position.x + 1)
+  // }
+  // bat.position.x = width - 100;
+  // bat.position.y = R + 100;
+  // bat.rotation = 0;
+}
+drawBat = () => {
 
-    if (mouseIsPressed) {
-    const dx = 20; //x dimension of image
-    const dy = -20; //y dimension of image
+  // console.log("X Pos: " + bat.position.x)
+  //   console.log("Y Pos: " + bat.position.x)
+  if (mouseIsPressed) {
+    const magX = accelerationX / 5;
+    const dx = 3; //x dimension of image
+    const dy = 0.25; //y dimension of image
     //rotation of bat and arm image, pivoted at center of image
-    swingAngle += -1; //value for rotating arm image
-    calcAngle += 1 //angle for calculating x & y translation of image (for pivoting image at elbow)
-    calcAngle = constrain(calcAngle, 0, 90); //constrain angle to min and max values
-    swingAngle = constrain(swingAngle, -90, 0); //constrain bat roation
+    swingAngle += -1 * 5; //value for rotating arm image
+    calcAngle = -swingAngle  //angle for calculating x & y translation of image (for pivoting image at elbow)
+    calcAngle = constrain(calcAngle, 0, 65); //constrain angle to min and max values
+    swingAngle = constrain(swingAngle, -65, 0); //constrain bat roation
     bat.rotation = swingAngle; //rotate image
     //translation of image so as to pivot
     //Trigonometric equations for circular positioning
     const transY = dx * tan(radians(calcAngle)); //image y displacement depending on angle of rotation
-    const transX = dy / (tan(radians(calcAngle))); //image X displacement depending on angle of rotation
-    //restrict position movement to angles less than 80
-    if (swingAngle > -80) {
-      bat.position.x += transX * 0.01; //moves image in x position, (0.01 to decrease magnitude**testing***)
-      bat.position.y += transY * 0.1; //moves image in y position, (magnitude testing)
-    }
+    const transX = -dy / (tan(radians(calcAngle))); //image X displacement depending on angle of rotation
 
+    console.log("X trans: " + transX)
+    console.log("Y trans: " + transY)
+    console.log("==========")
+
+    bat.position.x += transX; //moves image in x position, (0.01 to decrease magnitude**testing***)
+    bat.position.y += transY; //moves image in y position, (magnitude testing)
+    bat.position.y = constrain(bat.position.y, bat.position.y - 0.05, bat.position.y + 6.4)
+    bat.position.x = constrain(bat.position.x, bat.position.x - 0.11, bat.position.x + 1)
+    $('.accData').append($('<li>').text("----> X Acc: " + magX))
+    $('.angleData').append($('<li>').text("----> Tangent: " + tan(radians(calcAngle))))
   }
-
 }
--- public/script/sketch.js
+++ public/script/sketch.js
@@ -2,25 +2,25 @@
 // Spring drawing constants for top bar
 const springHeight = 32;
 let read = true,
-  left, 
+  left,
   right,
   maxHeight = 800,
   minHeight = 100;
 
 // Spring simulation constants
 const M = 8,  // Mass
-    K = 0.2,  // Spring constant
-    D = 0.92, // Damping
-    R = 600;  // Rest position
+  K = 0.2,  // Spring constant
+  D = 0.92, // Damping
+  R = 600;  // Rest position
 
 // Spring simulation variables
 let ps = R,   // Position
-    vs = 0.0, // Velocity
-    as = 0,   // Acceleration
-    f = 0;    // Force
+  vs = 0.0, // Velocity
+  as = 0,   // Acceleration
+  f = 0;    // Force
 
 //arm & bat swing variables
-let batX, batY, swingAngle, calcAngle;
+let bat, batX, batY, swingAngle, calcAngle;
 
 //----preload images----
 function preload() {
@@ -29,32 +29,33 @@ function preload() {
 
 function setup() {
   var myCanvas = createCanvas(800, 1000);
-  myCanvas.parent('canvas'); 
+  myCanvas.parent('canvas');
   // ----------setup spring----------
   rectMode(CORNERS);
   noStroke();
-  left = width/2 - 100;
-  right = width/2 + 100;
+  left = width / 2 - 100;
+  right = width / 2 + 100;
   //-----------setup bat swing---------
   calcAngle = 0; //
   swingAngle = 0;
   batX = width - 100; //x-position of bat sprite, to right side of screen
-  batY = R + 100; //y-position of bat sprite, equal to pinata resting
-  bat = createSprite(batX, batY, 20, 100);
+  batY = R + 10; //y-position of bat sprite, equal to pinata resting
+  imageMode(CORNERS);
+  bat = createSprite(batX, batY, 50, 200)
   bat.shapeColor = color(128);
   bat.addImage(arm)
 }
 
 
 function draw() {
-  background(200,200,200,200);  
-  
+  background(200, 200, 200, 200);
+
   // ---spring---
   updateSpring();
   drawSpring();
 
   //---swing animation---
-  batSwing(); //found in batSwing.js
+  drawBat(); //found in batSwing.js
 
   //display sprites
   drawSprites();
@@ -64,20 +65,20 @@ function drawSpring() {
   // Draw base
   fill(0.2);
   var baseWidth = 20;
-  rect(width/2 - baseWidth, 0, width/2 + baseWidth, ps + springHeight);
+  rect(width / 2 - baseWidth, 0, width / 2 + baseWidth, ps + springHeight);
 
   // Set color and draw top bar
-    fill(600);
+  fill(600);
 
-  rect(left, ps + springHeight , right, ps + springHeight + 20);
+  rect(left, ps + springHeight, right, ps + springHeight + 20);
 }
 
 function updateSpring(mag, alpha) {
-  mag= -mag;
+  mag = -mag;
 
   // Update the spring position
-  if ( mag > -30 ) {
-    f = -K * ( ps - R ); // f=-ky
+  if (mag > -30) {
+    f = -K * (ps - R); // f=-ky
     as = f / M;          // Set the acceleration, f=ma == a=f/m
     vs = D * (vs + as);  // Set the velocity
     ps = ps + vs;        // Updated position
@@ -91,8 +92,8 @@ function updateSpring(mag, alpha) {
 
   // Set and constrain the position of top bar
   if (mag < -30 && ready) {
-    $('.accData').append($('<li>').text("Y acc: " + alpha))
-    ps = mag*0.5
+    // $('.accData').append($('<li>').text("Y acc: " + alpha))
+    ps = mag * 0.5
     ps = constrain(ps, minHeight, maxHeight);
   }
 }
