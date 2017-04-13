68b7edbd4351a9777c538c549d3f1a707ccbf12f
Alejandro Harb <alejandroharb@gmail.com>
2017-04-13 14:00:56 -0500

    added prototype of functional bat movement, with mousePress, and position reset

--- public/script/batSwing.js
+++ public/script/batSwing.js
@@ -1,47 +1,43 @@
 restoreBatPos = () => {
-  // const dx = 3; //x dimension of image
-  // const dy = 0.25; //y dimension of image
-  // const transY = dx * tan(radians(calcAngle)); //image y displacement depending on angle of rotation
-  // const transX = -dy / (tan(radians(calcAngle))); //image X displacement depending on angle of rotation
-  // for (var i = swingAngle; i < 0; i++) {
-  //   swingAngle += 5
-  //   bat.position.x += transX; //moves image in x position, (0.01 to decrease magnitude**testing***)
-  //   bat.position.y += transY; //moves image in y position, (magnitude testing)
-  //   bat.position.y = constrain(bat.position.y, bat.position.y - 0.05, bat.position.y + 6.4)
-  //   bat.position.x = constrain(bat.position.x, bat.position.x - 0.11, bat.position.x + 1)
-  // }
-  // bat.position.x = width - 100;
-  // bat.position.y = R + 100;
-  // bat.rotation = 0;
+  swingAngle = 0;
+  calcAngle = 0;
+  bat.position.x = width - 100;
+  bat.position.y = R + 100;
+  bat.rotation = 0;
 }
 drawBat = () => {
 
-  // console.log("X Pos: " + bat.position.x)
-  //   console.log("Y Pos: " + bat.position.x)
   if (mouseIsPressed) {
     const magX = accelerationX / 5;
     const dx = 3; //x dimension of image
     const dy = 0.25; //y dimension of image
     //rotation of bat and arm image, pivoted at center of image
-    swingAngle += -1 * 5; //value for rotating arm image
+    swingAngle += -1; //value for rotating arm image
     calcAngle = -swingAngle  //angle for calculating x & y translation of image (for pivoting image at elbow)
     calcAngle = constrain(calcAngle, 0, 65); //constrain angle to min and max values
     swingAngle = constrain(swingAngle, -65, 0); //constrain bat roation
     bat.rotation = swingAngle; //rotate image
+
     //translation of image so as to pivot
     //Trigonometric equations for circular positioning
     const transY = dx * tan(radians(calcAngle)); //image y displacement depending on angle of rotation
     const transX = -dy / (tan(radians(calcAngle))); //image X displacement depending on angle of rotation
 
-    console.log("X trans: " + transX)
-    console.log("Y trans: " + transY)
-    console.log("==========")
-
     bat.position.x += transX; //moves image in x position, (0.01 to decrease magnitude**testing***)
     bat.position.y += transY; //moves image in y position, (magnitude testing)
-    bat.position.y = constrain(bat.position.y, bat.position.y - 0.05, bat.position.y + 6.4)
-    bat.position.x = constrain(bat.position.x, bat.position.x - 0.11, bat.position.x + 1)
+    bat.position.y = constrain(bat.position.y, batY - 1.25, batY + 4.4);
+    // bat.position.x = constrain(bat.position.x, batX - 0.11, batX + 2);
     $('.accData').append($('<li>').text("----> X Acc: " + magX))
     $('.angleData').append($('<li>').text("----> Tangent: " + tan(radians(calcAngle))))
   }
+  if (swingAngle < -64) {
+    restoreBatPos();
+  }
 }
+/* --todo:
+1. make bat swing only after a positive x acc, with min magnitude
+2. restore bat position after swing.
+3. allow bat y-movement depending on user's swing direction
+  3a. if user swing with positive y-acc, have bat move up
+  3b. if y-acc nega. bat moves down
+*/
