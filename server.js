c0509c8ee1ca4f480055e23a71fa40b6697d1739
michaelrogers <contact@mhrogers.com>
2017-04-19 20:36:24 -0500

    Moved device route into a mvc configuration.

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("./public"));
 
-app.get('/control-device/:gameId/:playerId/:playerSelection', function(req, res) {
-  const [gameId, playerId, playerSelection] = [req.params.gameId, req.params.playerId, req.params.playerSelection]; 
-  // console.log('yeah boyyyy', gameId, playerId, playerSelection);
-  res.sendFile(path.join(__dirname, 'public/fone.html'))
-  // res.end('good stuff')
-})
+// app.get('/control-device/:gameId/:playerId/:playerSelection', function(req, res) {
+//   const [gameId, playerId, playerSelection] = [req.params.gameId, req.params.playerId, req.params.playerSelection]; 
+//   // console.log('yeah boyyyy', gameId, playerId, playerSelection);
+//   res.sendFile(path.join(__dirname, 'public/fone.html'))
+//   // res.end('good stuff')
+// })
 
 const routes = require('./routes');
 for (let route in routes) {
